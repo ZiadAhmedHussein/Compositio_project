a37e97e44857c62c996b50bceddc259c01870612
ziad <ebedziad@gmail.com>
2022-12-28 04:41:39 +0200

    add project files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Compositio_project
---
+++ js/test.js
@@ -0,0 +1,5 @@
+let navBarIconBotton = document.querySelector(".nav-icon");
+let navBar = document.querySelector(".nav-bar");
+navBarIconBotton.onclick = ()=> {
+    navBar.classList.toggle("hidden");
+} 
